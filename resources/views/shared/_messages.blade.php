44943fab609257994a90cdd0c083dd0859dcddb5
dengao <2916012032@qq.com>
2019-06-27 17:29:29 +0800

    基础页面结构

---
+++ resources/views/shared/_messages.blade.php
@@ -0,0 +1,7 @@
+@foreach(['success','info','warning','danger'] as $msg)
+@if(session()->has($msg))
+<div class="flash flash-message">
+  <p class="alert alert-{{ $msg }}">{{ session()->get($msg) }}</p>
+</div>
+@endif
+@endforeach
